46c5c2e83a46fc5eb62d9f958a21dd2c40d14d71
johnnyM41 <halldemetrice32@gmail.com>
2023-02-07 23:39:51 +0000

    done

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,11 +8,13 @@ public class Application {
      */
     public static void main(String[] args) {
         Lab inst = new Lab();
+        
         System.out.println("An uninstantiated Object is a 'null' value in Java.");
         System.out.println("If we try to use a null Object like an instantiated Object,");
         System.out.println("We would probably get a NullPointerException, which would crash this program.");
         System.out.println("For instance, attempting to compare an Object to itself:");
         Object obj = inst.instantiateANewObject();
         System.out.println(obj.equals(obj));
+        return;
     }
 }
